86e1c6e9eb762f4b9cf85cfc49edf0cd6fe3da72
ShrayankM <shraymist@gmail.com>
2021-03-26 21:24:00 +0530

    added AI simple hill climbing(tsp)

---
+++ Artificial Intelligence/testing.py
@@ -0,0 +1,36 @@
+import random
+
+def randomSolution(tsp):
+    cities = list(range(len(tsp)))
+    solution = []
+
+    for i in range(len(tsp)):
+        randomCity = cities[random.randint(0, len(cities) - 1)]
+        solution.append(randomCity)
+        cities.remove(randomCity)
+
+    return solution
+
+def routeLength(tsp, solution):
+    routeLength = 0
+    for i in range(len(solution)):
+        routeLength += tsp[solution[i - 1]][solution[i]]
+    return routeLength
+
+def hillClimbing(tsp):
+    currentSolution = randomSolution(tsp)
+    currentRouteLength = routeLength(tsp, currentSolution)
+
+
+def main():
+    tsp = [
+        [0, 400, 500, 300],
+        [400, 0, 300, 500],
+        [500, 300, 0, 400],
+        [300, 500, 400, 0]
+    ]
+
+    print(hillClimbing(tsp))
+
+if __name__ == "__main__":
+    main()
