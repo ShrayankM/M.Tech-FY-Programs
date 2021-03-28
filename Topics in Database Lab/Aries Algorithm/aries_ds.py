c4dcf0d1351e49b5478f5beea922412676459872
ShrayankM <shraymist@gmail.com>
2021-03-28 20:07:25 +0530

    added aries data structures

---
+++ Topics in Database Lab/Aries Algorithm/aries_ds.py
@@ -0,0 +1,63 @@
+
+#* Data-Structures
+
+LOG_MAIN = {}
+LOG_DISK = {}
+
+class LogRecord:
+    def __init__(self, LSN, prevLSN, TxnId, type, dataItem, before, after, undoNext):
+        self.LSN = LSN
+        self.prevLSN = prevLSN
+        self.TxnId = TxnId
+        self.type = type
+        self.dataItem = dataItem
+        self.before = before
+        self.after = after
+        self.undoNext = undoNext
+    
+    def __str__(self):
+        return str(self.LSN) + " " + str(self.prevLSN) + " " + str(self.TxnId) 
+
+class Page:
+    def __init__(self, pageId, data):
+        self.pageId = pageId
+        self.pageLSN = None
+        self.recLSN = None
+        self.data = data
+    
+    def __str__(self):
+        return str(self.pageId) + " " + str(self.pageLSN) + " " + str(self.recLSN) + " " + str(self.data) 
+    
+    def search_data(self, d):
+        if self.data.get(d): return self.pageId
+        return -1
+    
+    def get_data_value(self, dId):
+        return self.data[dId]
+
+class ActiveTransaction:
+    def __init__(self, id, status, lastLSN):
+        self.transactionId = id
+        self.status = status
+        self.lastLSN = lastLSN
+    
+    def __str__(self):
+        return str(self.transactionId) + " " + str(self.status) + " " + str(self.lastLSN)
+
+page_table = {
+    'A': '1',
+    'B': '1',
+    'C': '2',
+    'D': '2'
+} 
+
+main_memory = {}
+
+disk_memory = {
+    '1': Page('1', {'A': 5, 'B': 6}),
+    '2': Page('2', {'C': 10, 'D': 9})
+}
+
+active_transaction_table = {}
+
+temp_values = {}
