ded3afcc04aabb58974ceb3bc7fb490cd0fc78c8
ShrayankM <shraymist@gmail.com>
2021-03-29 12:49:53 +0530

    analysis phase till commit operation

--- Topics in Database Lab/Aries Algorithm/aries_v1.py
+++ Topics in Database Lab/Aries Algorithm/aries_v1.py
@@ -1,4 +1,5 @@
 import aries_ds as ds
+import copy
 
 def get_value(ins, a, b):
     if (ins.find('+') > -1): return a + b
@@ -26,7 +27,7 @@ def execute_instructions():
                 transactionId = ins[ins.find('(') + 1:ins.find(')')] 
 
                 log_record = ds.LogRecord(LSN, None, transactionId, 'BEGIN', '-', 0, 0, None)
-                ds.LOG_DISK[LSN] = log_record
+                ds.LOG_MAIN[LSN] = log_record
                 tRecord = ds.ActiveTransaction(transactionId, 'UNDO', LSN)
                 ds.active_transaction_table[transactionId] = tRecord
                 LSN += 1
@@ -40,7 +41,61 @@ def execute_instructions():
                 if ds.main_memory.get(pageId): continue
                 else: 
                     page = ds.disk_memory.get(pageId)
-                    ds.main_memory[pageId] = page
+                    ds.main_memory[pageId] = copy.deepcopy(page)
+            
+            if (operation == 'WRITE'):
+                data_item = ins[ins.find('(') + 1:ins.find(',')]
+                transactionId = ins[ins.find(' ') + 1:ins.find(')')]
+                prevLSN = ds.active_transaction_table[transactionId].lastLSN
+                pageId = ds.page_table[data_item]
+                before = ds.main_memory[pageId].get_data_value(data_item)
+                after = ds.temp_values[data_item]
+                log_record = ds.LogRecord(LSN, prevLSN, transactionId, 'UPDATE', data_item, before, after, None)
+                ds.active_transaction_table[transactionId].lastLSN = LSN
+                ds.LOG_MAIN[LSN] = log_record
+                ds.main_memory[pageId].data[data_item] = after
+                ds.main_memory[pageId].pageLSN = LSN
+
+                if ds.dirty_page_table.get(pageId):
+                    LSN += 1
+                else:
+                    ds.dirty_page_table[pageId] = LSN
+                    ds.main_memory[pageId].recLSN = LSN 
+                    LSN += 1
+            
+            if (operation == 'COMMIT'):
+                transactionId = ins[ins.find('(') + 1:ins.find(')')]
+                prevLSN = ds.active_transaction_table[transactionId].lastLSN
+                ds.active_transaction_table[transactionId].lastLSN = LSN
+                log_record = ds.LogRecord(LSN, prevLSN, transactionId, 'COMMIT', '-', '-', '-', '-')
+                ds.LOG_MAIN[LSN] = log_record
+
+                for LSN, log_record in ds.LOG_MAIN.items():
+                    ds.LOG_DISK[LSN] = log_record
+                
+                ds.LOG_MAIN.clear()
+                ds.flushedLSN = LSN
+                LSN += 1
+
+                #* We can know start flushing pages to main disk
+                page_ids = []
+                for pageId, recLSN in ds.dirty_page_table.items():
+                    if recLSN <= ds.flushedLSN:
+                        data_items = ds.main_memory[pageId].data
+                        ds.disk_memory[pageId].data = data_items
+                        page_ids.append(pageId)
+                
+                for id in page_ids:
+                    ds.dirty_page_table.pop(id)
+                
+                prevLSN = ds.active_transaction_table[transactionId].lastLSN
+                log_record = ds.LogRecord(LSN, prevLSN, transactionId, 'END', '-', '-', '-', '-')
+                ds.LOG_MAIN[LSN] = log_record
+                LSN += 1
+
+                #* Removing completed transactions from active_transaction_table
+                ds.active_transaction_table.pop(transactionId)
+ 
         else:
             updating_data = ins[0]
             a = ins[ins.find('[') + 1]
@@ -69,16 +124,48 @@ def execute_instructions():
 if __name__ == '__main__':
     execute_instructions()
 
+    # return tabulate(data, headers = ["LSN", "prevLSN", "TxnId", "type", "data", "before", "after", "undoNext"])
+    main_records = []
+    print("  Log Records[On MAIN]")
+    for LSN, logR in ds.LOG_MAIN.items():
+        main_records.append(logR.get_record())
+    print(ds.tabulate(main_records, headers = ["LSN", "prevLSN", "TxnId", "type", "data", "before", "after", "undoNext"]))
 
-    # print("Log Records")
-    # for LSN, logR in ds.LOG_DISK.items():
-    #     print(logR)
-    
-    # # print("\n")
-    # print("Active Transaction Table")
-    # for tId, trans in ds.active_transaction_table.items():
-    #     print(trans)
-    
-    # print("Main Memory")
-    # for pageId, page in ds.main_memory.items():
-    #     print(page)
+    print()
+    disk_records = []
+    print("  Log Records[On DISK]")
+    for LSN, logR in ds.LOG_DISK.items():
+        disk_records.append(logR.get_record())
+    print(ds.tabulate(disk_records, headers = ["LSN", "prevLSN", "TxnId", "type", "data", "before", "after", "undoNext"]))
+
+    print()
+    print("  Main Memory")
+    pages = []
+    for pageId, page in ds.main_memory.items():
+        pages.append(page.get_page())
+    print(ds.tabulate(pages, ["pageId", "pageLSN", "recLSN", "data"]))
+
+    print()
+    print("  Disk Memory")
+    pages = []
+    for pageId, page in ds.disk_memory.items():
+        pages.append(page.get_page())
+    print(ds.tabulate(pages, ["pageId", "pageLSN", "recLSN", "data"]))
+
+    print()
+    print("  Dirty Page Table")
+    dirty_pages = []
+    for pageId, recLSN in ds.dirty_page_table.items():
+        dirty_pages.append([pageId, recLSN])
+    print(ds.tabulate(dirty_pages, headers = ["PageId", "recLSN"]))
+
+    print()
+    print("Active Transaction Table")
+    transactions = []
+    for transactionId, transaction in ds.active_transaction_table.items():
+        transactions.append(transaction.get_active_transaction())
+    print(ds.tabulate(transactions, headers = ["TransactionId", "status", "lastLSN"]))
+
+    # print("FLUSHED_LSN ", ds.flushedLSN)
+
+    # print(ds.temp_values)
