72d20b910cfc8a4cd9634e7052abdf0909560de1
ShrayankM <shraymist@gmail.com>
2021-04-24 10:43:47 +0530

    updated merge-sort join to handle double duplicates

--- Topics in Database Lab/#6 Hash and Sorted Natural Join/sort_based_join.py
+++ Topics in Database Lab/#6 Hash and Sorted Natural Join/sort_based_join.py
@@ -1,8 +1,5 @@
 from tabulate import tabulate
 
-def hash_function(h):
-    return h % 10
-
 def sort_for_B(L):
     return L[1]
 
@@ -10,10 +7,10 @@ def sort_for_A(L):
     return L[0]
 
 table_A = [
-    ['CustId', 'CustName'],
+    ['CustomerId', 'CustomerName'],
     [1, 'C1'],
     [2, 'C2'],
-    [3, 'C3']
+    [3, 'C3'],
 ]
 
 table_B = [
@@ -33,14 +30,43 @@ table_B = sorted(table_B[1:], key=sort_for_B)
 table_A = sorted(table_A[1:], key=sort_for_A)
 
 joined_table = list()
+primary_key_A = True
+primary_key_B = False
+
 #* Natural Join Phase
-j = 0
-for i in range(len(table_A)):
-    while j < len(table_B):
-        if table_A[i][0] == table_B[j][1]:
-            joined_table.append(list(table_A[i] + table_B[j]))
-            j = j + 1
-        else:
-            break
+#* Gives Correct Output if the common attribute in table_A is primary key
+
+if primary_key_A:
+    j = 0
+    for i in range(len(table_A)):
+        while j < len(table_B):
+            if table_A[i][0] == table_B[j][1]:
+                joined_table.append(list(table_A[i] + table_B[j]))
+                j = j + 1
+            else:
+                break
+else:
+    #* Grouping A
+    groups_A = dict()
+    a = 0
+    while a < len(table_A):
+        value = table_A[a][0]
+        groups_A[value] = list()
+        groups_A[value].append(table_A[a])
+
+        k = a + 1
+        while k < len(table_A) and value == table_A[k][0]:
+            groups_A[value].append(table_A[k])
+            k = k + 1
+        a = k
+    
+    #* Mapping table_B to groups in A (Cross product among groups)
+    for i in range(0, len(table_B)):
+        value = table_B[i][1]
+        records_A = groups_A[value]
+        
+        for r in records_A:
+            joined_table.append(list(r + table_B[i]))
+
 
 print(tabulate(joined_table, headers))
